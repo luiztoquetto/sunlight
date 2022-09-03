6f72517ad06d3c9cbe8fe126e75d73a05cd3e45b
Leonardo Dimarchi <leonardodimarchi@outlook.com>
2022-09-03 16:07:25 -0300

    feature(*): added capacitor native runtime

---
+++ android/app/src/main/java/com/sunlight/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.sunlight.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sunlight.app',
+  appName: 'sunlight',
+  webDir: 'dist/sunlight',
+  bundledWebRuntime: false
+};
+
+export default config;
