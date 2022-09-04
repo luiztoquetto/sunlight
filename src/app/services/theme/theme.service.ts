086a61a16f72cca238b2a913d0008967636312a9
luiz toquetto <luiz.toquetto@facens.br>
2022-09-04 18:46:38 -0300

    feat(global.scss && app/theme/variables.scss && app/services/theme): added global variables and style theme service

---
+++ src/app/services/theme/theme.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, Observable } from "rxjs";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ThemeService {
+  private isDarkThemeSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+
+  public setTheme(theme: 'dark' | 'light'): void {
+    this.isDarkThemeSubject.next(theme === 'dark');
+  }
+
+  public isDarkTheme$(): Observable<boolean> {
+    return this.isDarkThemeSubject.asObservable();
+  }
+}
