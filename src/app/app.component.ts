274cfc260909205975a845e4a9d4b92952f5f6ca
luiz toquetto <luiz.toquetto@facens.br>
2022-10-27 20:56:38 -0300

    feat(components/header): adding header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,4 +23,5 @@ export class AppComponent implements OnDestroy {
   public ngOnDestroy(): void {
     this.isDarkThemeSubscription.unsubscribe();
   }
+
 }
--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -1,19 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { CardComponent } from './card.component';
+import { HeaderComponent } from './header.component';
 
-describe('CardComponent', () => {
-  let component: CardComponent;
-  let fixture: ComponentFixture<CardComponent>;
+describe('HeaderComponent', () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CardComponent ]
+      declarations: [ HeaderComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CardComponent);
+    fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
