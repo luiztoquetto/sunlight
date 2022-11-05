31e21fb22feddc9aeea6a5b037bb2c4c2b4d8134
luiz toquetto <luiz.toquetto@facens.br>
2022-11-05 17:33:25 -0300

    Finalizando o header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { HeaderEnum } from "../../models/enums/header.enum";
+import { Component, Input } from '@angular/core';
+import { Router } from "@angular/router";
+import { HeaderEnum, HeaderNavigationEnum } from "../../models/enums/header.enum";
 
 @Component({
   selector: 'app-header',
@@ -8,8 +9,30 @@ import { HeaderEnum } from "../../models/enums/header.enum";
 })
 export class HeaderComponent {
 
-  public current: HeaderEnum = HeaderEnum.TABS;
+  constructor(
+    private readonly router: Router,
+  ) { }
+
+  @Input()
+  public goTo: string = '';
+
+  @Input()
+  public condominiumName: string = 'aaa';
+
+  public currentHeader: HeaderEnum = HeaderEnum.TABS;
 
   public headerEnum: typeof HeaderEnum = HeaderEnum;
+
+  public navigation: HeaderNavigationEnum = HeaderNavigationEnum.GENERAL;
+
+  public headerNavigationEnum: typeof HeaderNavigationEnum = HeaderNavigationEnum;
+
+  public async redirectTo(): Promise<void> {
+    await this.router.navigateByUrl(this.goTo);
+  }
+
+  public changeNavigation(currentNavigation: HeaderNavigationEnum): void {
+    this.navigation = currentNavigation;
+  }
 
 }
